b36f0f1f36da2d7306d5fe7acdea71baf633da95
Eric Lin <eric_lin@college.harvard.edu>
2019-03-17 23:54:01 -0400

    Rudimentary API call

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Website for searching images using NASA's API.
 Super user credentials:  
 Username/Email: super_user@test.com  
 Password: super_user_password  
+
+
+Functionality:  
+1. Search Function.  
+2. View image metadata for individual search results by clicking on the pictures.  
+3. Advanced search with refinement by media type, description, location, and date range (year_start and year_end).   
--- image_search/views.py
+++ image_search/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+import requests		# for API calls
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'image_search/test.html')	# render search html
+	# Dictionary to pass params to API
+	payload = {
+		# Search query
+		'q': 'apollo 11',
+		# Refinement by filters
+		'media_type': 'image',
+		'description': 'moon landing',
+		'location': 'kennedy space center',
+		'year_start': '1999',
+		'year_end': '2009',
+	}
+
+	# url call to NASA API 
+	url = 'https://images-api.nasa.gov/search'
+
+	r = requests.get(url, params= payload)	# requests call
+	print(r.url)
+	print(r.text)
+
+	return render(request, 'image_search/search.html')	# render search html
