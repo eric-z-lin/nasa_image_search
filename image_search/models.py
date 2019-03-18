0b0e5e4f2aaa3bcb749ba03e14943190f195a290
Eric Lin <eric_lin@college.harvard.edu>
2019-03-18 19:33:09 -0400

    Implemented API calls, metadata, images.

--- image_search/models.py
+++ image_search/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):		# to store favorite images
+	nasa_id = models.CharField(max_length=32)	# Nasa ID allows us to easily 
+
+	def __str__(self):
+		return self.nasa_id
+
+class Search(models.Model):		# to store past searches
+	search_query = models.CharField(max_length=32)	
+
+	def __str__(self):
+		return self.search_query
+
+	class Meta:
+		verbose_name_plural = 'searches'
--- image_search/views.py
+++ image_search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests		# for API calls
+from .models import Image, Search
 
 # Create your views here.
 
@@ -10,17 +11,56 @@ def index(request):
 		'q': 'apollo 11',
 		# Refinement by filters
 		'media_type': 'image',
-		'description': 'moon landing',
+		'description': 'eight apollo Astronauts',
 		'location': 'kennedy space center',
-		'year_start': '1999',
+		'year_start': '2009',
 		'year_end': '2009',
 	}
 
-	# url call to NASA API 
-	url = 'https://images-api.nasa.gov/search'
+	past_searches = list(Search.objects.values_list('search_query', flat = True))	# get past search results
 
-	r = requests.get(url, params= payload)	# requests call
-	print(r.url)
-	print(r.text)
+	url = 'https://images-api.nasa.gov/search'	# url call to NASA API 
 
-	return render(request, 'image_search/search.html')	# render search html
+	r = requests.get(url, params= payload).json()	# requests to get json
+
+	# r is a dictionary that has 'collection': 'items', 'metadata', 'version,' and 'href'
+	num_hits = r['collection']['metadata']['total_hits']	# number of items returned
+
+	# Example dictionary in items_list
+	# {
+	# 	"href":"https://images-assets.nasa.gov/image/KSC-99pp0855/collection.json",
+	# 	"data":[{
+	# 			"description":"KENNEDY SPACE CENTER, FLA. -- Former Apollo 11 astronaut...",
+	# 			"location":"Kennedy Space Center, FL",
+	# 			"title":"KSC-99pp0855",
+	# 			"nasa_id":"KSC-99pp0855",
+	# 			"media_type":"image",
+	# 			"date_created":"1999-07-16T00:00:00Z",
+	# 			"center":"KSC"
+	# 	}],
+	# 	"links":[{
+	# 		"href":"https://images-assets.nasa.gov/image/KSC-99pp0855/KSC-99pp0855~thumb.jpg",
+	# 		"render":"image",
+	# 		"rel":"preview"
+	# 	}]
+	# }	
+
+	# Simplify the list of metadata
+	metadata_list = []	# list of dictionaries for metadata
+	for d in r['collection']['items']:	# items is a list of dictionaries
+		# Select relevant metadata from item dictionary
+		image_metadata = {
+			'title': d['data'][0]['title'],
+			'media_type': d['data'][0]['media_type'],
+			'description': d['data'][0]['description'],	# long paragraph of text
+			'location': d['data'][0]['location'],
+			'date_created': d['data'][0]['date_created'][:10],
+			'nasa_id': d['data'][0]['nasa_id'],			# nasa_id of object
+			'media_link': d['links'][0]['href'],		# href link to media
+		}
+		metadata_list.append(image_metadata)
+
+	# Pass to template html
+	context = {'metadata_list': metadata_list, 'past_searches': past_searches}
+
+	return render(request, 'image_search/search.html', context)	# render search html
